6696bdd69da45424e0f7a87287eb4602ea41c90c
Christian <Christian.Block@yahoo.com>
2015-07-16 14:44:25 -0700

    added and configured ReactJS

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module OpenSourceCsDegree
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # For testing React components
+    config.react.addons = true
   end
 end
